0515dd7ccc8319293ffe6aac84b1634d3141aa32
BarigelaSaiCharan <barigelasaicharan6@gmail.com>
2025-01-03 21:15:23 +0530

    Add files via upload

---
+++ Sai.py
@@ -0,0 +1,9 @@
+
+integer_variable = 10  
+float_variable = 3.14  
+string_variable = "Hello, World!" 
+boolean_variable = True  
+print("Integer Variable:", integer_variable, "| Type:", type(integer_variable))
+print("Float Variable:", float_variable, "| Type:", type(float_variable))
+print("String Variable:", string_variable, "| Type:", type(string_variable))
+print("Boolean Variable:", boolean_variable, "| Type:", type(boolean_variable))
